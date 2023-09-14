dfd6fb26e86ef5cb1e6e40b3ea6ef86857f02357
lonewolf <thechampthapa@gmail.com>
2023-09-14 22:27:47 +0530

    ui part for welcome page done

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,30 @@
 import {Stack} from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack/>
+
+    // load custom fonts
+    const [fontsLoaded] = useFonts({
+        DMBold : require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium : require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular : require("../assets/fonts/DMSans-Regular.ttf"),
+    })
+
+    // only remove the splash screen when fonts are loaded
+    const onLayoutRootView = useCallback( async () => {
+        if(fontsLoaded)
+        {
+            await SplashScreen.hideAsync();
+        }
+    },[fontsLoaded]);
+
+    if(!fontsLoaded) return null;
+
+    return <Stack onLayout = {onLayoutRootView}/>
 }
 
 export default Layout;
